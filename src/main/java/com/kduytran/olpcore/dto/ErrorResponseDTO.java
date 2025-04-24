0492d520dddc6dbc3fe93ec9e96596f289a7bcc2
kduytran443 <trankhanhduy18@gmail.com>
2025-04-25 03:13:42 +0700

    Update version 1.0.5

--- src/main/java/com/kduytran/olpcore/dto/ErrorResponseDTO.java
+++ src/main/java/com/kduytran/olpcore/dto/ErrorResponseDTO.java
@@ -10,6 +10,16 @@ public class ErrorResponseDTO {
     private String errorMessage;
     private LocalDateTime errorTime;
 
+    public ErrorResponseDTO() {
+    }
+
+    public ErrorResponseDTO(String apiPath, HttpStatus errorCode, String errorMessage, LocalDateTime errorTime) {
+        this.apiPath = apiPath;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.errorTime = errorTime;
+    }
+
     public String getApiPath() {
         return apiPath;
     }
