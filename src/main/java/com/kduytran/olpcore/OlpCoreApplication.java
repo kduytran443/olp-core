a171046acd61379cef32fce4d40aa765c05cd08e
kduytran443 <trankhanhduy18@gmail.com>
2025-04-25 00:45:48 +0700

    Init project

---
+++ src/main/java/com/kduytran/olpcore/OlpCoreApplication.java
@@ -0,0 +1,13 @@
+package com.kduytran.olpcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlpCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlpCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kduytran/olpcore/OlpCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kduytran.olpcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlpCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
