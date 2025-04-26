ac1f7d84d07634d7beb56a89dc1973fd8451a260
kduytran443 <trankhanhduy18@gmail.com>
2025-04-26 21:46:02 +0700

    1.1.1

--- olp-common/src/main/java/com/kduytran/olpcommon/validation/CommonValidatorService.java
+++ olp-common/src/main/java/com/kduytran/olpcommon/validation/CommonValidatorService.java
@@ -1,6 +1,7 @@
 package com.kduytran.olpcommon.validation;
 
 import com.kduytran.olpcommon.exception.ResourceAlreadyExistedException;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.lang.reflect.Field;
 import java.util.List;
@@ -29,6 +30,15 @@ public class CommonValidatorService {
                 );
             }
         }
+    }
+
+    public static <T> Specification<T> fieldEquals(String fieldName, Object fieldValue, String id) {
+        return (root, query, criteriaBuilder) ->
+                Optional.ofNullable(id)
+                        .map(value ->
+                                criteriaBuilder.and(criteriaBuilder.equal(root.get(fieldName), fieldValue),
+                                criteriaBuilder.notEqual(root.get("id"), value)))
+                        .orElseGet(() -> criteriaBuilder.equal(root.get(fieldName), fieldValue));
     }
 
     private Object getFieldValue(Object object, String fieldName) {
